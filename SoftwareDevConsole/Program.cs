d7bf9c3c304b0d2d3b2be61b37afb50dbd95c51c
nadiia_aksonova <nadiakswork@gmail.com>
2022-01-04 20:06:43 +0200

    Start project Creating a project structure

--- SoftwareDevConsole/Program.cs
+++ SoftwareDevConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 
 namespace SoftwareDevConsole
 {
@@ -7,6 +8,7 @@ namespace SoftwareDevConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ SoftwareDevTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SoftwareDevTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
